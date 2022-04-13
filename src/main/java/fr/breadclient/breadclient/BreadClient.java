451d25172abc3f70484f005dda0172b3454ca818
SorenDeveloppement <sorenel@yahoo.com>
2022-04-13 14:59:18 +0200

    pom.xml changes

--- src/main/java/fr/breadclient/breadclient/BreadClient.java
+++ src/main/java/fr/breadclient/breadclient/BreadClient.java
@@ -1,10 +1,21 @@
 package fr.breadclient.breadclient;
 
+import javax.swing.*;
+
 public class BreadClient {
 
     private static final BreadClient INSTANCE = new BreadClient();
 
     public static BreadClient getInstance() {
         return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("javafx.application.Application");
+        }
+        catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "JavaFX is not installed on this computer \n " + e.getMessage(), "JavaFX not found", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
