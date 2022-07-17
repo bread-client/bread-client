3d36e162f6c4323d39ccabebe393713640fc2a21
SorenDeveloppement <63002111+SorenDeveloppement@users.noreply.github.com>
2022-07-17 22:50:24 +0200

    Adding CosmeticsCore to project

---
+++ src/main/java/fr/breadclient/breadclient/cosmetcs/CosmeticsCore.java
@@ -0,0 +1,11 @@
+package fr.breadclient.breadclient.cosmetics;
+
+import fr.galaglow.marketapi.product;
+
+public class CosmeticsCore {
+
+    public Cosmetics() {
+        Product cosmeticProduct = new Product(); 
+    }
+
+}
